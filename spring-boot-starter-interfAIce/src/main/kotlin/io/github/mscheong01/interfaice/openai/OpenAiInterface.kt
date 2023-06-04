99e3b5199af3b883eaa7869af637bc644bed6677
Minsoo Cheong <icycle0409@snu.ac.kr>
2023-06-05 00:47:05 +0900

    fix OpenAiInterface annotation

--- spring-boot-starter-interfAIce/src/main/kotlin/io/github/mscheong01/interfaice/openai/OpenAiInterface.kt
+++ spring-boot-starter-interfAIce/src/main/kotlin/io/github/mscheong01/interfaice/openai/OpenAiInterface.kt
@@ -1,3 +1,13 @@
 package io.github.mscheong01.interfaice.openai
 
-annotation class OpenAiInterface
+import org.springframework.core.annotation.AliasFor
+import org.springframework.stereotype.Component
+
+@Target
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Component
+annotation class OpenAiInterface(
+    @get:AliasFor(annotation = Component::class)
+    val value: String = ""
+)
