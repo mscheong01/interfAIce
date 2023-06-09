92ecb79cb840994ff8d59232ed11ce695fcb3c4a
Minsoo Cheong <icycle0409@snu.ac.kr>
2023-06-09 22:10:29 +0900

    remove unneeded configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,4 +4,3 @@ include("interfAIce-spring-boot-starter")
 include("examples")
 include("examples:kotlin-basic")
 include("examples:java-basic")
-findProject(":examples:java-basic")?.name = "java-basic"
